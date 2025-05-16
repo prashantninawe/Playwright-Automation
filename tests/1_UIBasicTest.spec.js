e1b72c9d24fc34dd0d5d60a720f873b8a92c8c02
Prashant Ninawe <ninawe.prashant@yahoo.com>
2025-05-16 20:03:44 +0530

    Create playwright repo

---
+++ tests/1_UIBasicTest.spec.js
@@ -0,0 +1,10 @@
+// import { test, expect } from '@playwright/test'
+const {test} = require('@playwright/test')
+
+test('First Playwright test', async ()=>
+{
+// playwright code goes here
+// step 1: Open the browser
+// step 2: Open the URL
+// step 3: Click on the button
+});
